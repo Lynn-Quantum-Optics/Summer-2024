acbfe2df7df3c7227554f8c0eb13739801dcd494
oscars47 <orsa2020@mymail.pomona.edu>
2023-06-08 14:02:48 -0700

    fixed xgb sweep

--- oscar/machine_learning/ml_hypersweep.py
+++ oscar/machine_learning/ml_hypersweep.py
@@ -5,7 +5,7 @@ import numpy as np
 from xgboost import XGBClassifier
 
 ## compute accuracy ##
-def evaluate_perf(model, method, X_train, Y_train, X_test, Y_test):
+def evaluate_perf(model, X_train, Y_train, X_test, Y_test):
     ''' Function to measure accuracy on both train and test data.
     Params:
         model: trained model
@@ -25,12 +25,12 @@ def evaluate_perf(model, method, X_train, Y_train, X_test, Y_test):
         if Y_train[i][np.argmax(y_pred)]==1: # if the witness is negative, i.e. detects entanglement
             N_correct_train+=1
 
-    if method == 'witness':
-        Ud = Y_test.sum(axis=1) # undetectables: count the number of states w negative witness value
-        return [N_correct_test / (len(Y_pred_test) - len(Ud[Ud==0])), N_correct_train / (len(Y_pred_train) - len(Ud[Ud==0]))]
-    elif method == 'entangled':
-        return [N_correct_test / len(Y_pred_test), N_correct_train / len(Y_pred_train)]
-        
+    # if method == 'witness':
+    #     Ud = Y_test.sum(axis=1) # undetectables: count the number of states w negative witness value
+    #     return [N_correct_test / (len(Y_pred_test) - len(Ud[Ud==0])), N_correct_train / (len(Y_pred_train) - len(Ud[Ud==0]))]
+    # elif method == 'entangled':
+    #     return [N_correct_test / len(Y_pred_test), N_correct_train / len(Y_pred_train)]
+    return [N_correct_test / len(Y_pred_test), N_correct_train / len(Y_pred_train)]
 
 
 #######################################################
@@ -39,37 +39,40 @@ def evaluate_perf(model, method, X_train, Y_train, X_test, Y_test):
 
 xgb_sweep_config = {
     "method": "bayes",
-    "metric": {"name": "mse", "goal": "minimize"},
+    "metric": {"name": "val_loss", "goal": "minimize"},
     "parameters": {
-        "max_depth": {"values": [1, 2, 5, 10]},
-        "learning_rate": {"distribution": "uniform", "min": 1e-6, "max": 0.9},
-        "n_estimators": {"distribution": "uniform", "min": 1000, "max": 5000},
-        "early_stopping": {"distribution": "uniform", "min": 5, "max": 30}
-        },
+        "max_depth": {"distribution": "int_uniform", "min":  1, "max": 10},
+        "learning_rate": {"distribution": "uniform", "min": 1e-5, "max": 0.9},
+        "n_estimators": {"distribution": "int_uniform", "min":  500, "max": 5000},
+        "early_stopping": {"distribution": "int_uniform", "min": 5, "max": 30}
+    },
     }
 
-def train_xgb(method, X_train, Y_train, X_test, Y_test):
+def train_xgb():
     ''' Function to run wandb sweep for XGBOOST. 
     Adapted from https://github.com/wandb/examples/blob/master/examples/wandb-sweeps/sweeps-xgboost/xgboost_tune.py 
     params:
         method: 'witness' or 'entangled' for prediction method
         data: 'train' or 'test' for which data to evaluate on
     '''
-    wandb.init() # initialize wandb client
+    wandb.init(config=xgb_sweep_config) # initialize wandb client
 
     # define the model
     model = XGBClassifier(
         max_depth=wandb.config.max_depth,
         learning_rate=wandb.config.learning_rate,
         n_estimators=wandb.config.n_estimators,
+        # max_depth=int(wandb.config.max_depth),
+        # learning_rate=wandb.config.learning_rate,
+        # n_estimators=int(wandb.config.n_estimators),
     )
 
     # fit the model
-    model.fit(X_train, Y_train, tree_method='hist', early_stopping_rounds = wandb.config.early_stopping, random_state=42, eval_set=[(X_test, Y_test)], callbacks=[wandb.xgboost.WandbCallback()])
-
+    model.fit(X_train, Y_train, early_stopping_rounds = wandb.config.early_stopping, eval_set=[(X_test, Y_test)], callbacks=[wandb.xgboost.WandbCallback(log_model=True)])
+    # early_stopping_rounds = int(wandb.config.early_stopping)
     # log test accuracy to wandb
-    acc = evaluate_perf(model, method, X_train, Y_train, X_test, Y_test)[0]
-    wandb.log({"acc": acc})
+    val_acc = evaluate_perf(model, X_train, Y_train, X_test, Y_test)[0]
+    wandb.log({"val_acc": val_acc})
 
 def custom_train_xgb(method, X_train, Y_train, X_test, Y_test, max_depth=6, learning_rate=0.3, n_estimators=1000, early_stopping=10):
     ''' Function to run XGBOOST with custom hyperparameters.
@@ -93,20 +96,30 @@ def custom_train_xgb(method, X_train, Y_train, X_test, Y_test, max_depth=6, lear
 
 #######################################################
 ## NN ##
+
+
+
 def train_nn(X_train, Y_train, X_test, Y_test):
-    # code here
-   pass
+    pass
 
 
 ## run on our data ##
 if __name__=='__main__':
     import pandas as pd
-    # load data here
+    from os.path import join
 
+    from train_prep import prepare_data
+
+    # load data here
+    JS_DATA_PATH = 'jones_simplex_data'
+    X_train, Y_train, X_test, Y_test, _ , _ = prepare_data(datapath=join(JS_DATA_PATH, 'simplex_100000_0.csv'), savename='simp_100k', method='witness_s')
+    
     def run_sweep(do_xgb=True):
         if do_xgb:
-            sweep_id = wandb.sweep(xgb_sweep_config, project="Lynn Quantum Optics")
+            sweep_id = wandb.sweep(xgb_sweep_config, project="Lynn Quantum Optics3")
             wandb.agent(sweep_id=sweep_id, function=train_xgb)
         else:
-            sweep_id = wandb.sweep(nn_sweep_config, project="Lynn Quantum Optics")
+            sweep_id = wandb.sweep(nn_sweep_config, project="Lynn Quantum Optics-NN")
             wandb.agent(sweep_id=sweep_id, function=train_nn)
+    
+    run_sweep(do_xgb=True)
