a69b9676b80dacc32e167e27d524a9f6158e7cd7
Oscar Scholin <orsa2020@mymail.pomona.edu>
2023-06-09 14:56:24 -0700

    updated jones decom

--- oscar/machine_learning/jones.py
+++ oscar/machine_learning/jones.py
@@ -5,7 +5,8 @@ import numpy as np
 from scipy.optimize import minimize
 
 # special methods for density matrices #
-from rho_methods import check_conc_min_eig, is_valid_rho,get_fidelity
+from rho_methods import *
+from random_gen import *
 
 ## jones matrices ##
 def R(alpha): return np.matrix([[np.cos(alpha), np.sin(alpha)], [-np.sin(alpha), np.cos(alpha)]])
@@ -15,13 +16,13 @@ def get_QP(phi): return np.matrix(np.diag([1, np.e**(phi*1j)]))
 B = np.matrix([[0, 0, 0, 1], [1, 0,0,0]]).T
 def init_state(beta, gamma): return np.matrix([[np.cos(beta),0],[0,np.e**(gamma*1j)*np.sin(beta)]])
 
-def get_Jrho_C(angles):
+def get_Jrho_C(angles, check=False):
     ''' Jones matrix with *almost* current setup, just adding one QWP on Alice. Computes the rotated polarization state using the following setup:
         UV_HWP -> QP -> BBO -> A_QWP -> A_Detectors
                             -> B_HWP -> B_QWP -> B_Detectors
         params:
-            angles: list of angles for HWP1, HWP2, QWP1, QWP2, QP
-            beta, gamma: angles for init polarization state
+            angles: list of angles for init polarization state (beta, gamma), HWP1, HWP2, QWP1, QWP2, QP
+            check: boolean to check if density matrix is valid; don't call when minimizing bc throwing an error will distrupt minimzation process. the check is handled in the while statement in jones_decomp -- for this reason, set to False by default
     '''
     Is = init_state(angles[0], angles[1])
     H1 = H(angles[2])
@@ -34,16 +35,24 @@ def get_Jrho_C(angles):
     P = np.kron(Q2, Q1 @ H2) @ B @ QP @ H1 @ Is
     rho = np.round(P @ P.H,2)
 
-    return rho
+    if check:
+        if is_valid_rho(rho):
+            return rho
+        else:
+            print(rho)
+            raise ValueError('Invalid density matrix')
 
-def get_Jrho_I(angles):
+    else:
+        return rho
+
+def get_Jrho_I(angles, check=False):
     ''' Jones matrix method with Ideal setup. Computes the rotated polarization state using the following setup:
         UV_HWP -> QP -> BBO -> A_HWP -> A_QWP -> A_QWP -> A_Detectors
                             -> B_HWP -> B_QWP -> B_QWP -> B_Detectors
 
         params:
-            angles: list of angles for HWP1, HWP2, HWP3, QWP1, QWP2, QWP3, QWP4, QWP5, QWP6
-            beta, gamma: angles for init polarization state
+            angles: list of angles for init polarzation state (beta, gamma), HWP1, HWP2, HWP3, QWP1, QWP2, QWP3, QWP4, QWP5, QWP6
+            check: boolean to check if density matrix is valid; don't call when minimizing bc throwing an error will distrupt minimzation process. the check is handled in the while statement in jones_decomp -- for this reason, set to False by default
     '''
     Is = init_state(angles[0], angles[1])
     H1 = H(angles[2])
@@ -61,7 +70,15 @@ def get_Jrho_I(angles):
     P = np.kron(H3 @ Q5 @ Q6, H2 @ Q3 @ Q4) @ B @Q2 @Q1 @ H1 @ Is
     rho = np.round(P @ P.H,2)
 
-    return rho
+    if check:
+        if is_valid_rho(rho):
+            return rho
+        else:
+            print(rho)
+            raise ValueError('Invalid density matrix')
+
+    else:
+        return rho
 
 def get_random_Jangles(setup='C'):
     ''' Returns random angles for the Jrho_C setup
@@ -106,7 +123,7 @@ def get_random_jones(setup='C'):
 
     return [rho, angles]
 
-def jones_decompose(targ_rho, setup = 'C', eps_min=0.8, eps_max=0.99, N = 20000, verbose=False):
+def jones_decompose(targ_rho, setup = 'C', eps_max=0.99, N = 30000, verbose=False):
     ''' Function to decompose a given density matrix into jones matrices
     params:
         targ_rho: target density matrix
@@ -130,7 +147,7 @@ def jones_decompose(targ_rho, setup = 'C', eps_min=0.8, eps_max=0.99, N = 20000,
         func = get_Jrho_I
         bounds = [(0, np.pi/2), (0, 2*np.pi), (0, np.pi/4), (0, np.pi/4), (0, np.pi/4), (0, np.pi/2), (0, np.pi/2),(0, np.pi/2), (0, np.pi/2), (0, np.pi/2), (0, np.pi/2) ]
     else:
-        raise ValueError('Invalid setup. Must be either "C" or "I"')
+        raise ValueError('Invalid setup. Must be either "C" or "I". You have f{setup}.')
 
     def loss_fidelity(angles, targ_rho):
         ''' Function to quantify the distance between the targ and pred density matrices'''
@@ -147,10 +164,14 @@ def jones_decompose(targ_rho, setup = 'C', eps_min=0.8, eps_max=0.99, N = 20000,
     rho = func(best_angles)
     
     # iterate eiher until we hit max bound or we get a valid rho with fidelity above the min
-    max_best_fidelity = fidelity
-    max_best_angles = best_angles
+    if is_valid_rho(rho):
+        max_best_fidelity = fidelity
+        max_best_angles = best_angles
+    else:
+        max_best_fidelity = 0
+        max_best_angles = []
     n=0
-    while n < N and (not(is_valid_rho(rho)) or  (is_valid_rho(rho) and fidelity<eps_min)):
+    while n < N and (not(is_valid_rho(rho)) or  (is_valid_rho(rho) and fidelity<eps_max)):
         try:
             n+=1
             if verbose: 
@@ -164,12 +185,9 @@ def jones_decompose(targ_rho, setup = 'C', eps_min=0.8, eps_max=0.99, N = 20000,
             fidelity = get_fidelity(func(best_angles), targ_rho)
             rho = func(best_angles)
 
-            if fidelity > max_best_fidelity:
+            if (fidelity > max_best_fidelity) and is_valid_rho(rho): # have to be a valid density matrix to assign maxes
                 max_best_fidelity = fidelity
                 max_best_angles = best_angles
-
-            if fidelity > eps_max: # if we get a good enough result, break early
-                break
 
         except KeyboardInterrupt:
             print('interrupted...saving best result so far')
@@ -179,7 +197,12 @@ def jones_decompose(targ_rho, setup = 'C', eps_min=0.8, eps_max=0.99, N = 20000,
         print('actual state', targ_rho)
         print('predicted state', func(max_best_angles) )
         print('fidelity', max_best_fidelity)
-    return max_best_angles, max_best_fidelity
+        print('projections', get_all_projections(func(max_best_angles)))
+
+    # compute projections in 12 basis states
+    HH, HV, VH, VV, DD, DA, AD, AA, RR, RL, LR, LL = get_all_projections(func(max_best_angles))
+
+    return max_best_angles, max_best_fidelity, [HH, HV, VH, VV, DD, DA, AD, AA, RR, RL, LR, LL], n
 
 
 if __name__=='__main__':
@@ -190,25 +213,88 @@ if __name__=='__main__':
     from sample_rho import *
     from random_gen import *
 
-    # initilize dataframe to store results
-    decomp_df = pd.DataFrame(columns=['state', 'angles', 'fidelity'])
+    def do_full_ex_decomp(setup,savename,bell=True, eritas=True, random=True, num_random=10, N=30000):
+        ''' Run example decompositions using the C setup.
+        Params:
+            setup: 'C' or ;'I'
+            savename: name of file to save results to
+            bell: whether to include bell states
+            num_random: number of random states to decompose
+            (optinonal) num_random: number of random states to decompose
+        '''
+        # initilize dataframe to store results
+        decomp_df = pd.DataFrame({'state':[], 'n':[], 'setup': [], 'init_pol':[], 'angles':[], 'fidelity':[], 'projH&V':[], 'projD&A':[], 'projR&L':[]})
+        states = []
+        states_names = []
 
-    # define test states
+        if bell:
+            states_bell = [PhiP, PhiM, PsiP, PsiM]
+            states_bell_names = ['PhiP', 'PhiM', 'PsiP', 'PsiM']
+            states += states_bell
+            states_names += states_bell_names
+
+        if eritas:
+            # get random Eritas states
+            states_E0_all = [get_random_E0() for i in range(num_random)]
+            states_E0 = [states_E0_all[i][0] for i in range(num_random)]
+            states_E0_names = [f'E0_{states_E0_all[i][1][0]}_{states_E0_all[i][1][1]}' for i in range(num_random)]
+
+            states+=states_E0
+            states_names+=states_E0_names
+
+            states_E1_all = [get_random_E1() for i in range(num_random)]
+            states_E1 = [states_E1_all[i][0] for i in range(num_random)]   
+            states_E1_names = [f'E1_{states_E1_all[i][1][0]}_{states_E1_all[i][1][1]}' for i in range(num_random)]
+
+            states+=states_E1
+            states_names+=states_E1_names
+         
+        if random:
+            states_RS_all = [get_random_simplex() for i in range(num_random)]
+            states_RS = [states_RS_all[i][0] for i in range(num_random)]
+            states_RS_names = [f'RS_{states_RS_all[i][1][0]}_{states_RS_all[i][1][1]}_{states_RS_all[i][1][2]}_{states_RS_all[i][1][3]}__{states_RS_all[i][1][4]}__{states_RS_all[i][1][5]}__{states_RS_all[i][1][6]}' for i in range(num_random)]
+       
+            states+=states_RS
+            states_names+=states_RS_names
+
+        for i in trange(len(states)):
+            try:
+                angles, fidelity, projections, n = jones_decompose(states[i], setup, N=N, verbose=True)
+                decomp_df = pd.concat([decomp_df, pd.DataFrame.from_records([{'state':states_names[i], 'n':n, 'setup': setup, 'init_pol':angles[:2],'angles':angles[2:], 'fidelity':fidelity, 'projH&V':projections[:4], 'projD&A':projections[4:8], 'projR&L':projections[8:]}])])
+            except: # if there's an error, save what we have
+                print('error! saving what we have')
+                decomp_df.to_csv(savename+'.csv')
+                
+        decomp_df.to_csv(savename+'.csv')
+        return decomp_df
+    setup=input('Enter setup (C or I): ')
+    while setup != 'C' and setup != 'I':
+        setup=input('Enter setup (C or I): ')
+    bell = bool(int(input('include bell states?')))
+    eritas = bool(int(input('include eritas states?')))
+    random = bool(int(input('include random states?')))
+    special = input('sepical name to append to file?')
+    savename = f'decomp_{setup}_{bell}_{eritas}_{random}_{special}'
+    print(setup, bell, eritas, random)
+
+    do_full_ex_decomp('I',bell=bell, eritas=eritas, random=random, savename=savename)
+        
+     # define test states
     # get random eta, chi: 
-    num_random = 10
-    eta_ls = np.random.rand(num_random)*np.pi/2
-    chi_ls = np.random.rand(num_random)*2*np.pi
+    # num_random = 10
+    # eta_ls = np.random.rand(num_random)*np.pi/2
+    # chi_ls = np.random.rand(num_random)*2*np.pi
 
-    states=[PhiP, PhiM, PsiP, PsiM, *[E_state0(eta_ls[i], chi_ls[i]) for i in range(num_random)], *[E_state1(eta_ls[i], chi_ls[i]) for i in range(num_random)], *[get_random_simplex()[0] for i in range(num_random)]]
-    states_names = ['PhiP', 'PhiM', 'PsiP', 'PsiM', *[f'E0_{eta_ls[i]}_{chi_ls[i]}' for i in range(num_random)], *[f'E1_{eta_ls[i]}_{chi_ls[i]}' for i in range(num_random)], *[f'RS_{get_random_simplex()[1]}' for i in range(num_random)]]
-    # do only C setup for now
-    setup = ['C', 'C', 'C', 'C', *[f'C_{i}' for i in range(num_random)], *[f'C_{i}' for i in range(num_random)], [f'C_{i}' for i in range(num_random)]]
+    # states=[PhiP, PhiM, PsiP, PsiM, *[E_state0(eta_ls[i], chi_ls[i]) for i in range(num_random)], *[E_state1(eta_ls[i], chi_ls[i]) for i in range(num_random)], *[get_random_simplex()[0] for i in range(num_random)]]
+    # states_names = ['PhiP', 'PhiM', 'PsiP', 'PsiM', *[f'E0_{eta_ls[i]}_{chi_ls[i]}' for i in range(num_random)], *[f'E1_{eta_ls[i]}_{chi_ls[i]}' for i in range(num_random)], *[f'RS_{get_random_simplex()[1]}' for i in range(num_random)]]
+    # # do only C setup for now
+    # setup = ['C', 'C', 'C', 'C', *['C' for i in range(num_random)], *['C' for i in range(num_random)], *['C' for i in range(num_random)]]
 
      # run decomposition
-    for i in trange(len(states)):
-        print('starting state', states_names[i], '...')
-        angles, fidelity = jones_decompose(states[i], setup[i], verbose=True)
-        decomp_df = pd.concat([decomp_df, pd.DataFrame.from_records([{'state': states_names[i], 'angles': angles, 'fidelity': fidelity}])])
+    # for i in trange(len(states)):
+    #     print('starting state', states_names[i], '...')
+    #     angles, fidelity = jones_decompose(states[i], setup[i], verbose=True)
+    #     decomp_df = pd.concat([decomp_df, pd.DataFrame.from_records([{'state': states_names[i], 'angles': angles, 'fidelity': fidelity}])])
 
     # states_C = [PhiP, PhiM, PsiP, PsiM, E_state0(eta_ls[0], chi_ls[0]), E_state0(eta_ls[1], chi_ls[1]), E_state0(eta_ls[2], chi_ls[2])]
     # states_names_C = ['PhiP', 'PhiM', 'PsiP', 'PsiM', 'E0_'+str(eta_ls[0])+'_'+str(chi_ls[0]), 'E0_'+str(eta_ls[1])+'_'+str(chi_ls[1]), 'E0_'+str(eta_ls[2])+'_'+str(chi_ls[2])]
@@ -220,7 +306,3 @@ if __name__=='__main__':
     # states_tot = states_C + states_I
     # names_tot = states_names_C + states_names_I
     # setup_tot = setup_C + setup_I
-
-        
-    # save results
-    decomp_df.to_csv('decomp_results.csv')
