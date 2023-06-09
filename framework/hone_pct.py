2a89d6b0bc5bf1855677c2c55632a28f6c6c5467
Alec Roberson <alectroberson@gmail.com>
2023-06-09 14:44:46 -0700

    updates to honing routine

--- framework/hone_pct.py
+++ framework/hone_pct.py
@@ -5,31 +5,35 @@ import numpy as np
 def hone_pct(m:Manager, component:str, target_pct_1:float, guess:float, delta:float, num_samp:int, samp_period:float, *bases):
     ''' Hone in on a percentage of detections in a given basis.
     '''
-    m.log('BEGIN SUBROUTINE: hone_pct')
-    # angles that we will take data at
-    xs = [guess - delta, guess, guess + delta]
-    m.log(f'Angles to test: {", ".join([str(x) for x in xs])}')
+    # note routine
+    m.log('BEGIN ROUTINE: hone_pct')
+    
+    # angles that we will take data at to start
+    xs_ = [guess - delta, guess, guess + delta] # xs that we *want* to hit
+    m.log(f'Initial angles to test: {", ".join([str(x) for x in xs_])}')
     
     # output coincidence rates, empty lists for each basis
+    xs = [[] for _ in bases] # actual angles we hit
     ys = [[] for _ in bases]
     y_errs = [[] for _ in bases]
 
     # take data at each angle for each basis. the basis motors are slower so we manipulate them minimally
     for i, b in enumerate(bases):
-        m.log(f'Taking data for basis {b}')
+        m.log(f'Setting measurement basis {b}')
         # set measurement basis
         m.meas_basis(b)
         # take data at each angle
-        for x in xs:
+        for x in xs_:
             m.log(f'Moving {component} to {x}')
             # set the component
-            m.configure_motors(**{component:x})
-            # take data
-            m.log('Taking data...')
+            xs.append(m.configure_motor(component, x))
             rate, unc = m.take_data(num_samp, samp_period, 'C4')
             # append to list
             ys[i].append(rate)
             y_errs[i].append(unc)
+    
+    # finished phase 1, print a note
+    m.log('Done with initial sweep.')
     
     # calculate percentage data based on rates
     ys = np.array(ys)
@@ -39,47 +43,73 @@ def hone_pct(m:Manager, component:str, target_pct_1:float, guess:float, delta:fl
     pct_errs = np.sqrt((np.sum(ys[1:], axis=0)*y_errs[0])**2 + ys[0]**2 * np.sum(y_errs**2, axis=0)) / np.sum(ys, axis=0)**2
 
     # print out some notes
-    for i, x in enumerate(xs):
+    for i, x in enumerate(xs_):
         m.log(f'{component} = {x} deg -> {pct_data[i]*100:.5f} ± {pct_errs[i]*100:.5f}% {bases[0]} detections')
     
-    # do a linear regression to find the angle that gives the target percentage
-    m.log('Doing linear regression to determine optimal angle')
-    (slope,intercept), _, _ = analysis.fit('line', xs, pct_data, pct_errs)
-    target_angle = (target_pct_1 - intercept) / slope
-    m.log(f'Calculated best angle: {target_angle} deg')
+    # loop to go until we get close enough
+    m.log('Moving to iterative regressions.')
+    loop_count = 0
+    while loop_count < 10:
+        m.log(f'Iterative regression, round {loop_count+1}.')
+        # do a linear regression to find the angle that gives the target percentage
+        (slope,intercept), _, _ = analysis.fit('line', xs_, pct_data, pct_errs)
+        target_angle = (target_pct_1 - intercept) / slope
+        m.log(f'Calculated best angle: {target_angle} deg')
+        
+        # obtain data for the target angle
+        m.log('Taking data at target angle...')
+        rates = []
+        uncs = []
+        m.configure_motors(**{component:target_angle})
+        for b in bases:
+            m.log(f'Configuring basis {b}')
+            m.meas_basis(b)
+            rate, unc = m.take_data(num_samp, samp_period, 'C4')
+            rates.append(rate)
+            uncs.append(unc)
 
-    
-    # obtain data for the target angle
-    m.log('Taking data for target angle...')
-    rates = []
-    uncs = []
-    m.configure_motors(**{component:target_angle})
-    for b in bases:
-        m.log(f'Configuring basis {b}')
-        m.meas_basis(b)
-        rate, unc = m.take_data(num_samp, samp_period, 'C4')
-        rates.append(rate)
-        uncs.append(unc)
+        rates = np.array(rates)
+        uncs = np.array(uncs)
+        
+        m.log(f'Calculating statistics for output')
+        # calculate percentage data based on rates
+        pct1 = rates[0]/np.sum(rates)
+        # disgusting error expression based on *the math*
+        pct1_unc = np.sqrt((np.sum(rates[1:])*uncs[0])**2 + rates[0]**2 * np.sum(uncs[1:]**2)) / np.sum(rates)**2
+        # calculate angle uncertainty (kinda)
+        angle_unc = abs(pct1_unc / slope)
+        # log the output values
+        m.log(f'target_angle = {target_angle}')
+        m.log(f'angle_unc = {angle_unc}')
+        m.log(f'pct1 = {pct1}')
+        m.log(f'pct1_unc = {pct1_unc}')
+        
+        # check if it's good enough
+        if abs(target_pct_1 - pct1) < pct1_unc:
+            m.log(f'pct1 is {abs(target_pct_1 - pct1)/pct1_unc:.3f} sigma from target -> terminating search')
+            break
+        else:
+            # not good enough, add this datapoint and try again
+            xs_ = np.append(xs_, [target_angle], axis=0)
+            pct_data = np.append(pct_data, [pct1], axis=0)
+            pct_errs = np.append(pct_errs, [pct1_unc], axis=0)
+            continue
 
-    rates = np.array(rates)
-    uncs = np.array(uncs)
-    
-    m.log(f'Calculating final statistics for output')
-    # calculate percentage data based on rates
-    pct1 = rates[0]/np.sum(rates)
-    # disgusting error expression based on *the math*
-    pct1_unc = np.sqrt((np.sum(rates[1:])*uncs[0])**2 + rates[0]**2 * np.sum(uncs[1:]**2)) / np.sum(rates)**2
-    
-    # calculate angle uncertainty (kinda)
-    angle_unc = abs(pct1_unc / slope)
-    
-    m.log(f'target_angle = {target_angle}')
-    m.log(f'angle_unc = {angle_unc}')
-    m.log(f'pct1 = {pct1}')
-    m.log(f'pct1_unc = {pct1_unc}')
-
-    m.log('END SUBROUTINE: hone_pct')
+    m.log('END ROUTINE: hone_pct')
     return target_angle, angle_unc, pct1, pct1_unc
+
+def min_det(m:Manager, component:str, guess:float, delta:float, num_sweep:int, samp:'tuple[int,float]'):
+    m.log('BEGIN ROUTINE: min_det')
+    x = []
+    y = []
+    yerr = []
+
+
+    for angle in np.linspace(guess-delta, guess+delta, num_sweep):
+
+
+    return theta
+    m.log('END ROUTINE: min_det')
 
 def main():
     # parameters
