8cfe8d6ce78edd383e305016e31b435fa8dcc681
Alec Roberson <alectroberson@gmail.com>
2023-06-20 21:30:50 -0700

    updates to drift analysis that have been in use for a while

--- framework/drift_analysis.py
+++ framework/drift_analysis.py
@@ -1,11 +1,22 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from core import analysis
 
 def acquire_data(all_data_file:str):
     # load data and initialize output
     all_data = pd.read_csv(all_data_file)
     new_data = {'time (min)':[], 'HH':[], 'VV':[], 'HH_err':[], 'VV_err':[]}
+
+    # get start and stop times
+    start_time = all_data.iloc[0]['start time (s)']
+    end_time = all_data.iloc[-1]['stop time (s)']
+
+    # get the samples per data point
+    num_samp_orig = all_data.iloc[0]['num samples (#)']
+
+    # collection period
+    collection_period = all_data.iloc[0]['period per sample (s)'] * num_samp_orig
 
     # loop to re-format data
     for i in range(len(all_data)):
@@ -23,7 +34,7 @@ def acquire_data(all_data_file:str):
             new_data['HH_err'].append(None)
             new_data['VV_err'].append(all_data.iloc[i]['C4 rate SEM (#/s)'])
 
-    return pd.DataFrame(new_data)
+    return pd.DataFrame(new_data), start_time, end_time, num_samp_orig, collection_period
 
 def aggregate_data(df:pd.DataFrame, num_samp:int, n_trial:int) -> pd.DataFrame:
     ''' Aggregates drift experiment data every num_samp data points.
@@ -73,24 +84,65 @@ def calculate_percentages(df:pd.DataFrame):
     return df
 
 if __name__ == '__main__':
-    # parameters
-    
-    ALL_DATA_FILE = 'DE1_all_data.csv'
-    OUTPUT_FILE = 'DE1_output_30min.csv'
-    AGGREGATE_EVERY = 5 # minutes
-    NUM_SAMP_ORIGINAL = 6 # number of samples in original data collection
-    PLOT_TITLE = f'Ratio of HH to HH and VV detections in the $\Phi^+$ state\nData taken in each basis for 30 seconds, every other minute for 5 hours\nData Averaged Every {AGGREGATE_EVERY} Minutes\n2023.06.12 17:36:15 - 2023.06.12 22:37:02'
+    # obtain the data
+    ALL_DATA_FILE = 'drift_experiment/DE2_all_data.csv'
+    df, start_time, end_time, num_samp_orig, collection_period = acquire_data(ALL_DATA_FILE)
 
-    df = acquire_data(ALL_DATA_FILE)
-    df = aggregate_data(df, AGGREGATE_EVERY // 2, NUM_SAMP_ORIGINAL)
+    # other parameters
+    AGGREGATE_EVERY = 2
+    EXPERIMENT_LENGTH = int(df.iloc[-1]['time (min)'] / 60) # hours
+    OUTPUT_PREFIX = 'DE2'
+    PLOT_TITLE = f'HH and VV detections of the $\Phi^+$ state ({start_time} - {end_time})\nData taken in each basis for {collection_period} seconds, every other minute for {EXPERIMENT_LENGTH} hours (avg. every {AGGREGATE_EVERY} min)'
+
+    # aggregate the data
+    df = aggregate_data(df, AGGREGATE_EVERY // 2, num_samp_orig)
     df = calculate_percentages(df)
 
-    # save the output data frame before plotting
-    df.to_csv(OUTPUT_FILE, index=False)
+    # df = df.iloc[:len(df)//3]
+    # df = df.iloc[len(df)//3:2*len(df)//3]
+    df = df.iloc[2*len(df)//3:]
 
-    # plot the data
-    plt.errorbar(x=df['time (min)'], y=df['pct HH'], yerr=df['pct HH err'], ms=5, fmt='ko-')
-    plt.xlabel('Time (minutes)')
-    plt.ylabel('Percentage of HH detections\n(of HH and VV)')
-    plt.title(PLOT_TITLE)
-    plt.show()
+    # fit a linear model to the data
+    params, errs = analysis.fit('line', df['time (min)'], df['pct HH'], df['pct HH err'])
+
+    # put together the plots
+    fig = plt.figure(figsize=(10, 10))
+
+    # HH and VV data together
+    ax = fig.add_subplot(411)
+    ax.errorbar(x=df['time (min)'], y=df['HH']+df['VV'], yerr=df['HH_err'] + df['VV_err'], fmt='bo-', ms=2)
+    ax.set_xlabel('Time (minutes)')
+    ax.set_ylabel('Count Rate (#/s)')
+    ax.set_title('Cummulative HH + VV Count Rate Over Time')
+
+    # VV - HH
+    ax = fig.add_subplot(412)
+    ax.errorbar(x=df['time (min)'], y=df['VV'] - df['HH'], yerr=df['HH_err'] + df['VV_err'], fmt='bo-', ms=2)
+    ax.set_xlabel('Time (minutes)')
+    ax.set_ylabel('Count Rate (#/s)')
+    ax.set_title('Difference in Count Rates (VV - HH) Over Time')
+
+    # HH and VV data separately
+    ax = fig.add_subplot(413)
+    ax.errorbar(x=df['time (min)'], y=df['HH'], yerr=df['HH_err'], fmt='go-', ms=2, label='HH')
+    ax.errorbar(x=df['time (min)'], y=df['VV'], yerr=df['VV_err'], fmt='bo-', ms=2, label='VV')
+    ax.set_xlabel('Time (minutes)')
+    ax.set_ylabel('Count Rate (#/s)')
+    ax.set_title('HH and VV Count Rates Over Time')
+    ax.legend()
+    
+    # percentage data
+    ax = fig.add_subplot(414)
+    ax.errorbar(x=df['time (min)'], y=df['pct HH'], yerr=df['pct HH err'], ms=2, fmt='bo-')
+    ax.set_xlabel('Time (minutes)')
+    ax.set_ylabel('Percentage of HH detections (%)')
+    ax.set_title('Percentage of HH Detections Over Time')
+    analysis.plot_func('line', params, df['time (min)'], ax, label=f'Linear Fit (slope = {params[0]*60:.5f}±{errs[0]*60:.5f} %/hr)', color='r')
+    ax.legend()
+
+    # overall title
+    fig.suptitle(PLOT_TITLE)
+    fig.tight_layout()
+    
+    # save the plot
+    plt.savefig(f'{OUTPUT_PREFIX}_plot.png', dpi=600)
