bccb0d32b67ab40f7060f0dbbce56ae9a239eea4
Alec Roberson <alectroberson@gmail.com>
2023-06-28 09:58:57 -0700

    updated state measurement script with the new method of finding phi

--- framework/state_measurement.py
+++ framework/state_measurement.py
@@ -43,6 +43,33 @@ def meas_HV(m:Manager, samp:Tuple[int,float]):
     VV, VV_unc = m.take_data(*samp, "C4")
     return [HH, HV, VH, VV], [HH_unc, HV_unc, VH_unc, VV_unc]
 
+def meas_DRL_RRL(m:Manager, samp:Tuple[int,float]):
+    ''' Takes data in (DR, DL, RR, RL) bases.
+    
+    Parameters
+    ----------
+    m : Manager
+        The manager object that is controlling the experiment.
+    samp : Tuple[int,float]
+        The sample parameters for each measurement.
+    
+    Returns
+    -------
+    list
+        The count rates for DR, DL, RR, and RL coincidences, in order.
+    list
+        The uncertainties for DR, DL, RR, and RL coincidences, in order.
+    '''
+    m.meas_basis("DR")
+    DR, DR_unc = m.take_data(*samp, "C4")
+    m.meas_basis("DL")
+    DL, DL_unc = m.take_data(*samp, "C4")
+    m.meas_basis("RR")
+    RR, RR_unc = m.take_data(*samp, "C4")
+    m.meas_basis("RL")
+    RL, RL_unc = m.take_data(*samp, "C4")
+    return [DR, DL, RR, RL], [DR_unc, DL_unc, RR_unc, RL_unc]
+
 def meas_DA_RL(m:Manager, samp:Tuple[int,float]):
     ''' Takes data in (DR, DL, AR, AL) basis.
     
@@ -70,6 +97,69 @@ def meas_DA_RL(m:Manager, samp:Tuple[int,float]):
     AL, AL_unc = m.take_data(*samp, "C4")
     return [DR, DL, AR, AL], [DR_unc, DL_unc, AR_unc, AL_unc]
 
+def meas_ab(m:Manager, samp:Tuple[int, float]) -> Tuple[Tuple[float, float], Tuple[float, float]]:
+    ''' Measure alpha and beta parameters of the state.
+
+    Parameters
+    ----------
+    m : Manager
+        The manager object running the experiment.
+    samp : Tuple[int, float]
+        The sampling parameters for measurements.
+    
+    Returns
+    -------
+    Tuple[float, float]
+        The alpha and beta parameters of the state.
+    Tuple[float, float]
+        Uncertainties in the alpha and beta parameters.
+    '''
+    # take data in H/V basis
+    (HH, HV, VH, VV), (HHu, HVu, VHu, VVu) = meas_HV(m, samp)
+    T_hv = HH + HV + VH + VV
+
+    # compute alpha
+    alpha = np.arctan(np.sqrt((VH+VV)/(HH+HV)))
+    # compute alpha uncertainty
+    alpha_unc = 1/(2*T_hv) * np.sqrt((HHu**2 + HVu**2) * (VH+VV)/(HH+HV) + (VHu**2 + VVu**2) * (HH+HV)/(VH+VV))
+
+    # compute beta
+    beta = np.arctan(np.sqrt((HV+VH)/(HH+VV)))
+    # compute uncertainty in beta
+    beta_unc = 1/(2*T_hv) * np.sqrt((HHu**2 + VVu**2) * (VH+HV)/(HH+VV) + (HVu**2 + VHu**2) * (HH+VV)/(HV+VH))
+
+    # return it all!
+    return (alpha, beta), (alpha_unc, beta_unc)
+
+def meas_phi(m:Manager, samp:Tuple[int, float]) -> Tuple[float, float]:
+    ''' Measure phi parameter of the state.
+
+    Parameters
+    ----------
+    m : Manager
+        The manager object running the experiment.
+    samp : Tuple[int, float]
+        The sampling parameters for measurements.
+    
+    Returns
+    -------
+    float
+        The phi parameter of the state.
+    float
+        Uncertainty in the phi parameter.
+    '''
+    # take data in the appropriate bases
+    (DR, DL, RR, RL), (DRu, DLu, RRu, RLu) = meas_DRL_RRL(m, samp)
+
+    # compute phi
+    u = (DL-DR)/(RR-RL)
+    phi = np.arctan(u)
+
+    # compute uncertainty in phi
+    unc = 1/(1+u**2) * 1/(RR-RL) * np.sqrt(DLu**2 + DRu**2 + u**2 * (RRu**2 + RLu**2))
+
+    return phi, unc
+
 def meas_all(m:Manager, samp:Tuple[int, float]) -> Tuple[Tuple[float, float, float], Tuple[float, float, float]]:
     ''' Measure alpha, beta, and phi parameters of the state.
 
@@ -87,31 +177,8 @@ def meas_all(m:Manager, samp:Tuple[int, float]) -> Tuple[Tuple[float, float, flo
     Tuple[float, float, float]
         Uncertainties in the alpha, beta, and phi parameters.
     '''
-    # take data in H/V basis
-    (HH, HV, VH, VV), (HHu, HVu, VHu, VVu) = meas_HV(m, samp)
-    T_hv = HH + HV + VH + VV
-    
-    # take data in DA/RL basis
-    (DR, DL, AR, AL), (DRu, DLu, ARu, ALu) = meas_DA_RL(m, samp)
-    T_dr = DR + DL + AR + AL
+    (a,b), (au, bu) = meas_ab(m, samp)
+    p, pu = meas_phi(m, samp)
+    return (a,b,p), (au, bu, pu)
 
-    # compute alpha
-    alpha = np.arctan(np.sqrt((VH+VV)/(HH+HV)))
-    # compute alpha uncertainty
-    alpha_unc = 1/(2*T_hv) * np.sqrt((HHu**2 + HVu**2) * (VH+VV)/(HH+HV) + (VHu**2 + VVu**2) * (HH+HV)/(VH+VV))
-
-    # compute beta
-    beta = np.arctan(np.sqrt((HV+VH)/(HH+VV)))
-    # compute uncertainty in beta
-    beta_unc = 1/(2*T_hv) * np.sqrt((HHu**2 + VVu**2) * (VH+HV)/(HH+VV) + (HVu**2 + VHu**2) * (HH+VV)/(HV+VH))
-    
-    # compute phi
-    u = (DR-DL-AR+AL)/(T_dr*np.sin(2*alpha))
-    phi = np.arcsin(u)
-
-    # compute uncertainty in phi
-    phi_unc = 2/(np.sqrt(1-u**2)*np.sin(2*alpha)) * np.sqrt((DL + AR)/T_dr * (DRu**2 + ALu**2) + (DR + AL)/T_dr * (DLu**2 + ARu**2) + u**2*np.cos(2*alpha)**2*alpha_unc**2)
-
-    # return it all!
-    return (alpha, beta, phi), (alpha_unc, beta_unc, phi_unc)
 
