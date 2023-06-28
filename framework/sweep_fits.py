ea03da859dfd12a7690dadbc6fcce0e3fa9ec4c7
Alec Roberson <alectroberson@gmail.com>
2023-06-28 14:58:01 -0700

    added working version of sweep_fits.py to test out

--- framework/sweep_fits.py
+++ framework/sweep_fits.py
@@ -55,11 +55,63 @@ def sweep_qp_phi(m:Manager, samp:Tuple[int,float], num_step:int, out_file:str=No
     print(f'b = {params[1]} ± {uncs[1]}')
     plt.show()
 
+def sweep_uvhwp_alph(m:Manager, samp:Tuple[int,float], num_step:int, out_file:str=None):
+    '''
+    Sweep the UVHWP while collecting alpha data.
+    '''
+
+    # gather coincidence data in the four relevant bases
+    pos_min = 0
+    pos_max = 50
+    angles = np.linspace(pos_min, pos_max, num_step)
+    m.meas_basis('HH')
+    HH, HHu = m.sweep('UVHWP', pos_min, pos_max, num_step, *samp)
+    m.meas_basis('HV')
+    HV, HVu = m.sweep('UVHWP', pos_min, pos_max, num_step, *samp)
+    m.meas_basis('VH')
+    VH, VHu = m.sweep('UVHWP', pos_min, pos_max, num_step, *samp)
+    m.meas_basis('VV')
+    VV, VVu = m.sweep('UVHWP', pos_min, pos_max, num_step, *samp)
+
+    # total count rates
+    T = HH + HV + VH + VV
+
+    # compute alpha
+    alpha = np.arctan(np.sqrt((VH+VV)/(HH+HV)))
+    alpha_unc = 1/(2*T) * np.sqrt((HHu**2 + HVu**2) * (VH+VV)/(HH+HV) + (VHu**2 + VVu**2) * (HH+HV)/(VH+VV))
+
+    # repackage data into a dataframe
+    df = pd.DataFrame({'UVHWP':angles, 'alpha':alpha, 'unc':alpha_unc})
+
+    # save if requested
+    if out_file is not None:
+        df.to_csv(out_file, index=False)
+
+    # error bar plot of the data
+    plt.errorbar(df['UVHWP'], df['alpha'], df['unc'], fmt='ro', ms=0.1)
+
+    # fit a secant function to the datas!
+    # params, uncs = analysis.fit('sin2', df['QP'], df['phi'], df['unc'], p0=[-1000, 500], bounds=([-1e5, -2000], [0, 2000]))
+    # analysis.plot_func('sec', params, df['QP'], label=f'${params[0]:.3f}\\sec(\\theta) + {params[1]:.3f}$')
+    
+    # plot axes and such
+    plt.xlabel('UV Half Wave Plate Angle (degrees)')
+    plt.ylabel('Alpha Parameter (degrees)')
+    plt.title(f'Alpha parameter as UVHWP plate\nis swept from {df["UVHWP"].min()} to {df["UVHWP"].max()} degrees')
+    plt.legend()
+
+    # print the results and show the plot
+    # print(f'a = {params[0]} ± {uncs[0]}')
+    # print(f'b = {params[1]} ± {uncs[1]}')
+    plt.show()
+
 if __name__ == '__main__':
     
     SAMP = (3,0.5)
     NUM_STEP = 30
 
-    sweep_qp_phi(None, SAMP, NUM_STEP, None)
+    m = Manager()
 
+    # sweep_qp_phi(None, SAMP, NUM_STEP, None)
+    sweep_uvhwp_alph(m, SAMP, NUM_STEP, 'uvhwp_alph_sweep.csv')
     
