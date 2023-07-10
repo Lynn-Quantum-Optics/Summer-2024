8737dc95ded95471bc91af93c92d52d66bce5987
oscars47 <orsa2020@mymail.pomona.edu>
2023-07-10 15:39:55 -0700

    added custom

--- oscar/machine_learning/ml_comp.py
+++ oscar/machine_learning/ml_comp.py
@@ -10,7 +10,7 @@ from multiprocessing import Pool, cpu_count
 from train_prep import prepare_data
 from rho_methods import compute_witnesses
 
-def eval_perf(model, name):
+def eval_perf(model, name, file_ls = ['roik_True_400000_t.csv', 'm_total.csv']):
     ''' Function to measure accuracy on new data from Roik and Matlab. Returns df.'''
     def get_labels(Y_pred):
         ''' Function to assign labels based on argmax per row'''
@@ -45,7 +45,6 @@ def eval_perf(model, name):
         print(N_correct / len(Y_pred))
         return N_correct / len(Y_pred), N_correct, len(Y_pred)
 
-    file_ls = ['roik_True_400000_t.csv', 'm_total.csv']
     # file_ls = ['../../S22_data/all_qual_102_tot.csv']
     p_df = pd.DataFrame()
     for file in file_ls:
@@ -53,34 +52,36 @@ def eval_perf(model, name):
         p_df = pd.concat([p_df,pd.DataFrame.from_records([{'model':name, 'file':file.split('_')[0], 'acc':acc, 'N_correct':N_correct, 'N_total':N_total}])])
     return p_df
 
-
-# define models
-new_model_path= join('random_gen', 'models', 'w_7_7')
-old_model_path = 'old_models'
-
-## load models ##
-
-# new models ---
-xgb = XGBRegressor()
-xgb.load_model(join(new_model_path, 'xgb_w_prob_9_r4.json'))
-
-# old models ---
-xgb_old = XGBRegressor()
-xgb_old.load_model(join(old_model_path, 'xgbr_best_3.json'))
-
-json_file = open(join(old_model_path, 'model_qual_v2.json'), 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-model_bl = keras.models.model_from_json(loaded_model_json)
-# load weights into new model
-model_bl.load_weights(join(old_model_path, "model_qual_v2.h5"))
+if __name__ == '__main__':
 
 
-## evaluate ##
-model_ls = [xgb, xgb_old, model_bl]
-model_names = ['xgb', 'xgb_old', 'bl_old']
-df = pd.DataFrame()
-for model, name in zip(model_ls, model_names):
-    df = pd.concat([df, eval_perf(model, name)])
-print('saving!')
-df.to_csv(join(new_model_path, 'model_perf.csv'))
+    # define models
+    new_model_path= join('random_gen', 'models', 'w_7_7')
+    old_model_path = 'old_models'
+
+    ## load models ##
+
+    # new models ---
+    xgb = XGBRegressor()
+    xgb.load_model(join(new_model_path, 'xgb_w_prob_9_r4.json'))
+
+    # old models ---
+    xgb_old = XGBRegressor()
+    xgb_old.load_model(join(old_model_path, 'xgbr_best_3.json'))
+
+    json_file = open(join(old_model_path, 'model_qual_v2.json'), 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    model_bl = keras.models.model_from_json(loaded_model_json)
+    # load weights into new model
+    model_bl.load_weights(join(old_model_path, "model_qual_v2.h5"))
+
+
+    ## evaluate ##
+    model_ls = [xgb, xgb_old, model_bl]
+    model_names = ['xgb', 'xgb_old', 'bl_old']
+    df = pd.DataFrame()
+    for model, name in zip(model_ls, model_names):
+        df = pd.concat([df, eval_perf(model, name)])
+    print('saving!')
+    df.to_csv(join(new_model_path, 'model_perf.csv'))
