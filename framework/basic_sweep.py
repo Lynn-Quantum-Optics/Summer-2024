55d112bf2d2ec658c8b3ef5ad84f0bd949b8029f
Alec Roberson <alectroberson@gmail.com>
2023-07-12 13:14:49 -0700

    adding basic_sweep (updated to work with new lab_framework package)

--- framework/basic_sweep.py
+++ framework/basic_sweep.py
@@ -3,26 +3,48 @@
 This file performs a basic sweep of one optical component through a range of angles, collecting data at even intervals.
 '''
 
-from core import Manager
+from lab_framework import Manager, analysis
 import matplotlib.pyplot as plt
 import numpy as np
 
 # this condition is REQUIRED for the multiprocessing that allows for the CCU to run parallel plotting with data collection
 if __name__ == '__main__':
+    '''
     # create a manager object
-    m = Manager(out_file='basic_sweep_data.csv')
+    m = Manager()
 
-    # configure DA measurement
+    # configure measurement basis and state preset
+    m.make_state('phi_plus')
     m.meas_basis('HV')
 
     # loop to collect data
-    for i, k in enumerate(np.linspace(-np.pi/3, np.pi/3, 30)):
-        print('Iteration', i+1, '/8')
-        m.take_data(5,2)
+    for i, angle in enumerate(np.linspace(-60, 60, 30)):
+        print('Beginning iteration', i+1, '/30')
+        m.B_C_HWP.goto(angle)
+        m.take_data(5,0.5)
     
+    df = m.output_data('basic_sweep_data.csv')
+
     m.shutdown()
+    '''
 
+    df = Manager.load_data('basic_sweep_data.csv')
+
+    # fit the data to a function
+    params = analysis.fit('sin2_sq', df['B_C_HWP'], df['C4'])
     
+    # plot the function
+    analysis.plot_func('sin2_sq', params, df['B_C_HWP'], label=f'${params[0].n:.3f}\\sin^2(2x + {params[1].n:.3f}) + {params[2].n:.3f}$')
 
-
-
+    # plot the data
+    analysis.plot_errorbars(df['B_C_HWP'], df['C4'], fmt='o', ms=0.1, label='Data')
+    
+    # setup plot labels and such
+    plt.xlabel('Bob\'s Creation HWP Angle')
+    plt.ylabel('HV Coincidence Count Rates (#/s)')
+    plt.title('Sweep of Bob\'s Creation HWP Angle (degrees)')
+    plt.legend()
+    
+    # save and show the plot
+    plt.savefig('basic_sweep_plot.png', dpi=600)
+    plt.show()
