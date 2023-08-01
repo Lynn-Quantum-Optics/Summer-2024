5ec278694a9b55a1294006e4a3b4831d5c4fe4b1
Alec Roberson <alectroberson@gmail.com>
2023-07-31 17:23:48 -0700

    updated plotting for AQWP data to be prettier

--- calibration/AQWP/AQWP_fit.py
+++ calibration/AQWP/AQWP_fit.py
@@ -29,28 +29,25 @@ if __name__ == '__main__':
     m.shutdown()
     '''
 
-    df = Manager.load_data(f'AQWP_sweep{TRIAL}.csv')
+    df = Manager.load_data('AQWP_sweep1.csv')
     
     # fit the function
-    params = analysis.fit('quadratic', df['A_QWP'], df['C4'])
+    params = analysis.fit(fit_func, df['A_QWP'], df['C4'], p0=[0, 90.1518, 2423, -46], bounds=[[-180, -180, -inf, -inf], [180, 180, inf, inf]], maxfev=1000)
+    # params = analysis.fit('quadratic', df['A_QWP'], df['C4'])
 
     # print fitted parameters
     print(f'Fit parameters = {params}')
 
-    # setup plot
-    fig = plt.figure(figsize=(6,4))
-    ax = fig.add_subplot(1,1,1)
-    
     # plotting
-    analysis.plot_func('quadratic', params, df['A_QWP'], color='b', linestyle='dashed', label=f'${params[1].n:.3f}(x-({params[0].n:.3f}))^2 + {params[2].n:.3f}$', alpha=0.3)
+    # analysis.plot_func('quadratic', params, df['A_QWP'], color='b', linestyle='dashed', label=f'Fit Function', alpha=0.3)
+    fig = plt.figure(figsize=(9,6))
+    ax = fig.add_subplot(1,1,1)
+    analysis.plot_func(fit_func, params, df['A_QWP'], color='b', linestyle='dashed', label=f'${params[2].n:.3f}[\\cos^2({params[1].n:.3f})+\\cos(2\\cdot{params[1].n:.3f})\\sin^2(2(\\theta{params[0].n:.3f}))/2]+{params[3].n:.3f}$', alpha=0.3)
     analysis.plot_errorbar(df['A_QWP'], df['C4'], ms=0.1, fmt='ro', capsize=2, label='Data')
-    
-    # labels and such
-    plt.title(f'Alice QWP Sweep')
     plt.xlabel('Alice\'s QWP Angle (degrees)')
     plt.ylabel('Count Rate (#/s)')
     plt.legend()
-
-    # save and show
-    plt.savefig(f'AQWP_sweep{TRIAL}.png', dpi=600)
+    # plt.title(f'Fit=${params[1].n:.3f}(x-{params[0].n:.3f})^2 + {params[2].n:.3f}$')
+    plt.title(f'Alice QWP Sweep')
+    plt.savefig(f'AQWP_fit.png', dpi=600)
     plt.show()
