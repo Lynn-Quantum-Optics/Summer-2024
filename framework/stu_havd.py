83dfffd731065638ac662cc6b1ea0b3f62086722
stkerr0 <stkerr@g.hmc.edu>
2024-07-10 15:32:20 -0700

    fixed the phase

--- framework/stu_havd.py
+++ framework/stu_havd.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
     # setup the phase sweep
     m.reset_output()
     #x_vals = np.linspace(*SWEEP_PARAMS[:3])
-    m.meas_basis('DD')
+    m.meas_basis('DR')
     m.configure_motors(C_UV_HWP =-112.41754451550933 +45)
     m.sweep("C_QP", -35, -1, 20, 5, 3) #Sometimes the minimum is near the edge of the bounds in which case you won't get a parabola/normal angle. 
     m.configure_motors(C_UV_HWP =-112.41754451550933 )
